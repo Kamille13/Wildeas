a851160ead4102e5c23d5a019262ee1828ced0dd
Sébastien <snx.dubois@gmail.com>
2019-03-15 16:46:54 +0100

    bouton retour

--- WiIdeas/app/src/main/java/com/example/wiideas/StartActivity.java
+++ WiIdeas/app/src/main/java/com/example/wiideas/StartActivity.java
@@ -39,6 +39,9 @@ public class StartActivity extends AppCompatActivity {
                 fromStartActivity.putExtra("toMainActivity", (Parcelable) userStarActivity);
                 startActivity(fromStartActivity);
 
+
+
+
             }
         });
 
